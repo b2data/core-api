cce73f08e48f87f7fa718b203512b2475870e5a7
Nikolai Turov <noreply@b2data.ton>
2025-04-01 14:11:05 +0000

    Updated from @b2data/core-backend

--- AuthApi.ts
+++ AuthApi.ts
@@ -46,14 +46,6 @@ export interface User {
   avatar?: string;
 }
 
-export enum AuthErrorCodes {
-  Api403 = "api:403",
-  Auth404 = "auth:404",
-  Auth401 = "auth:401",
-  Auth403 = "auth:403",
-  User404 = "user:404",
-}
-
 export interface VerifyAuthPayload {
   /** Selected Space ID */
   spaceId?: string;
